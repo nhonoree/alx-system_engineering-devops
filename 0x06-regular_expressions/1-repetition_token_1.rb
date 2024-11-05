2f530695f00280cc762077c5b4e1ed3e215bf0be
nhonoree <honoreniyirema@gmail.com>
2024-11-05 15:39:39 -0800

    updates

---
+++ 0x06-regular_expressions/1-repetition_token_1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.length != 1
+  puts "Usage: ruby 1-repetition_token_1.rb <string>"
+  exit
+end
+
+# Get the input string from command line arguments
+input_string = ARGV[0]
+
+# Define the regular expression to match a character repeated exactly twice
+regex = /^(.)\1$/
+
+# Check if the input string matches the regex
+if input_string.match?(regex)
+  puts "Match"
+else
+  puts "No match"
+end
